e737ed70a3ba3811b83427d1a86bf07c92ab49f5
SandroHvex <sandro.luiz@hvex.com.br>
2024-02-28 17:22:51 -0300

    :recycle: refactor: teste de exportacao multipages

--- src/index.ts
+++ src/index.ts
@@ -38,14 +38,55 @@ const generatePDF = async (
     console.error("Unable to get the target element.");
     return;
   }
-  const canvas = await html2canvas(targetElement, {
-    useCORS: options.canvas.useCORS,
-    logging: options.canvas.logging,
-    scale: options.resolution,
-    ...options.overrides?.canvas,
+
+  const childNodes = Array.from(targetElement.children);
+  const pages = [];
+
+  // Ensure content within child elements is loaded and rendered before capturing
+  await new Promise((resolve) => {
+    setTimeout(resolve, 0); // Schedule a microtask to allow for asynchronous loading
   });
-  const converter = new Converter(canvas, options);
-  const pdf = converter.convert();
+
+  for (const child of childNodes) {
+    if (child instanceof HTMLElement || child instanceof Text) {
+      // Process both HTML elements and text nodes
+      const canvas = await html2canvas(child as HTMLElement, {
+        useCORS: options.canvas.useCORS,
+        logging: options.canvas.logging,
+        scale: options.resolution,
+        ...options.overrides?.canvas,
+      });
+
+      if (canvas) { // Check if canvas is successfully created
+        const converter = new Converter(canvas, options);
+        const pdfPage = converter.convert();
+        console.log("Captured PDF page data:", pdfPage);
+        pages.push(pdfPage);
+      } else {
+        console.warn(`Failed to capture content for element: `, child);
+      }
+    } else {
+      console.warn(`Ignoring unsupported child node type: `, child?.constructor.name);
+    }
+  }
+
+  const pdf = new jsPDF();
+  const a4WidthInInches = 8.27;
+  const a4HeightInInches = 11.69;
+  const resolution = 72; // 72 pixels per inch
+  const widthInPixels = a4WidthInInches * resolution;
+  const heightInPixels = a4HeightInInches * resolution;
+
+  for (const page of pages) {
+    const imageData = page.output('datauristring');
+    const imageDataParts = imageData.split(",");
+    const base64ImageData = imageDataParts[1];
+  console.log("Image data to be added to PDF:", {base64ImageData}); 
+    pdf.addPage()
+    pdf.addImage(base64ImageData,'jpeg', 0, 0, 180, 180);
+  }
+
+  
   switch (options.method) {
     case "build":
       return pdf;
